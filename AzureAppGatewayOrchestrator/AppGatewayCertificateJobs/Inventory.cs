9e44602b3b2ec0d6f535a0c1077151c6ad91d78e
Morgan Gangwere <470584+indrora@users.noreply.github.com>
2026-06-30 14:33:49 -0700

    Merge 3.5.0 to main (#30)
    * [skip ci] Updated workflows to use Version 5
    
    comitted-by: Morgan Gangwere <morgan.gangwere@keyfactor.com>
    
    * 85863 add pam support (#29)
    
    * Added PAM Support
    
    * Update generated docs
    
    * Adding Mocks
    
    * Fixing up unit test
    
    * Added PAM Support for Bin extension
    
    * Finsihed changes for PAM support
    
    * Added unit tests, added license headers
    
    * Updated workflows to use Version 5
    
    * docs: auto-generate README and documentation [skip ci]
    
    * added net 10.0 support
    
    * docs: auto-generate README and documentation [skip ci]
    
    * Added missing schema value in manifest.json
    
    * fixed misspellings
    
    ---------
    
    Co-authored-by: Bob Pokorny <bpokorny@keyfactor.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    Co-authored-by: Joe VanWanzeele <jvanwanzeele@keyfactor.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Morgan Gangwere <470584+indrora@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bob Pokorny <bpokorny@keyfactor.com>
    Co-authored-by: Bob Pokorny <55611381+rcpokorny@users.noreply.github.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    Co-authored-by: Joe VanWanzeele <jvanwanzeele@keyfactor.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- AzureAppGatewayOrchestrator/AppGatewayCertificateJobs/Inventory.cs
+++ AzureAppGatewayOrchestrator/AppGatewayCertificateJobs/Inventory.cs
@@ -1,24 +1,20 @@
-// Copyright 2024 Keyfactor
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
+//  Copyright 2026 Keyfactor
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Azure.ResourceManager.Network.Models;
 using AzureApplicationGatewayOrchestratorExtension.Client;
 using Keyfactor.Logging;
 using Keyfactor.Orchestrators.Common.Enums;
 using Keyfactor.Orchestrators.Extensions;
+using Keyfactor.Orchestrators.Extensions.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace AzureApplicationGatewayOrchestratorExtension.AppGatewayCertificateJobs;
@@ -30,13 +26,28 @@ public class Inventory : IInventoryJobExtension
 
     ILogger _logger = LogHandler.GetClassLogger<Inventory>();
 
+    public IPAMSecretResolver _resolver;
+
+    public Inventory(IPAMSecretResolver resolver)
+    {
+            _resolver = resolver;
+    }
+
+    public Inventory()
+    {
+            
+    }
+
     public JobResult ProcessJob(InventoryJobConfiguration config, SubmitInventoryUpdate cb) 
     {
         _logger.LogDebug($"Beginning App Gateway Inventory Job");
 
         if (Client == null)
         {
-            Client = new AppGatewayJobClientBuilder<GatewayClient.Builder>()
+            var clientBuilder = new AppGatewayJobClientBuilder<GatewayClient.Builder>();
+            clientBuilder.resolver = _resolver;
+
+            Client = clientBuilder
                 .WithCertificateStoreDetails(config.CertificateStoreDetails)
                 .Build();
         }
